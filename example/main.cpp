c9dba156d26fbaac43f37197f8b0cdb8ebbb7cf7
Boris M <onilink_@hotmail.fr>
2015-10-08 23:38:35 +0200

    Create main.cpp

---
+++ example/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "evlog.h"
+
+int main()
+{
+    using namespace evlog;
+    using namespace std;
+    
+    CrashReportWindows report("crash_logs.bin", "PlanetCentauri.exe");
+    report.parseLogs();
+    
+    cout << "Captured logs: " << report.logs().size() << '\n';
+    
+    for(int i=0; i<report.logs().size() && i<10; ++i)
+    {
+        auto& log = report.logs()[i];
+        cout << log.program << ' ' << log.version.asString() << ' ' << hex << log.offset << '\n';
+    }
+    
+    cout << "\nNew filtered logs: \n";
+    for(auto& e: report.newErrors())
+    {
+        cout << e.version.asString() << ':' << e.offset << '\n';
+    }
+    
+    return 0;
+}
