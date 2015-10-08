e5e29ba6e8868ea90ddc6db07cbce4b7e43a77e3
Boris M <onilink_@hotmail.fr>
2015-10-08 23:34:18 +0200

    Create evlog.h

---
+++ lib/evlog.h
@@ -0,0 +1,85 @@
+#ifndef EVLOG_H_INCLUDED
+#define EVLOG_H_INCLUDED
+
+#include <string>
+#include <vector>
+
+namespace evlog
+{
+
+struct Version
+{
+    unsigned char n[4];
+    
+public:
+    Version();
+    Version(unsigned major, unsigned minor, unsigned build, unsigned revision);
+    Version(unsigned value);
+    Version(const std::string& str);
+    
+    unsigned value() const;
+    void set(unsigned major, unsigned minor, unsigned build, unsigned revision);
+    void set(unsigned value);
+    void set(const std::string& str);
+    
+    unsigned char major() const;
+    unsigned char minor() const;
+    unsigned char build() const;
+    unsigned char revision() const;
+    
+    unsigned char operator()(unsigned i) const;
+    unsigned char& operator()(unsigned i);
+    
+    bool operator< (const Version& o) const;
+    bool operator<=(const Version& o) const;
+    bool operator> (const Version& o) const;
+    bool operator>=(const Version& o) const;
+    bool operator==(const Version& o) const;
+    bool operator!=(const Version& o) const;
+    
+    std::string asString() const;
+};
+
+struct Crash
+{
+    std::string computer;
+    std::string program;
+    Version     version;
+    std::string error_from;
+    Version     version_dep;
+    unsigned    offset;
+    std::string log;
+};
+
+struct Error
+{
+    Version  version;
+    unsigned offset;
+    bool operator<(const Error& o) const;
+};
+
+class CrashReportWindows
+{
+    std::string _fname;
+    std::string _app_name;
+    
+    std::vector<Crash> _logs;
+    
+    std::vector<Error> _processed;
+    std::vector<Error> _new_errors;
+    
+    void dumpRecords(const std::vector<char>& buffer);
+    
+public:
+    CrashReportWindows(const std::string& fname, const std::string& app_name);
+    ~CrashReportWindows();
+    
+    bool parseLogs(unsigned buffer_size = 0x10000);
+    
+    const std::vector<Crash>& logs() const { return _logs; }
+    const std::vector<Error>& newErrors() const { return _new_errors; }
+};
+
+}
+
+#endif // EVLOG_H_INCLUDED
